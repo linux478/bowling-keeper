3cda247ac5f5a83040e35ea7e9c3c56429585544
Michael Phillips <linux478@gmail.com>
2021-09-09 22:45:48 -0400

    Create data.js

---
+++ data.js
@@ -0,0 +1,9 @@
+var bowlingData = {
+  "bowlers" = [{
+    "first_name" = "Michael"
+    "last_name" = "Phillips"
+    "scores" = [
+      {"date" = "2021-09-09", "games" = [null,127,171]}
+    ]
+  }]
+]

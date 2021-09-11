52183f81c867b6c168e4b098e8f09d1b5e783376
Michael Phillips <linux478@gmail.com>
2021-09-10 22:27:44 -0400

    Delete data.js
    The information was transfer to index.html

--- data.js
+++
@@ -1,9 +0,0 @@
-var bowlingData = {
-  "bowlers" = [{
-    "first_name" = "Michael"
-    "last_name" = "Phillips"
-    "scores" = [
-      {"date" = "2021-09-09", "games" = [null,127,171]}
-    ]
-  }]
-]
